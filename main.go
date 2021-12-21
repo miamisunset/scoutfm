47099f2361b81ceb437114336dc502ca4a86d6c8
Alex Yermolaev <ayermolaev@pm.me>
2021-12-20 23:11:50 -0500

    file browser frame

--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
+	"golang.org/x/term"
+	"log"
+	"os"
+	"strings"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"log"
 )
 
 type Styles struct {
+	App               lipgloss.Style
 	FileBrowserBorder lipgloss.Border
+	BorderColor       lipgloss.Color
 }
 
 func DefaultStyles() *Styles {
 	s := new(Styles)
 
-	s.FileBrowserBorder = lipgloss.Border{
-		Top:         "─",
-		Bottom:      "─",
-		Left:        "│",
-		Right:       "│",
-		TopLeft:     "╭",
-		TopRight:    "┬",
-		BottomLeft:  "├",
-		BottomRight: "┴",
-	}
+	s.App = lipgloss.NewStyle()
+
+	s.FileBrowserBorder = lipgloss.RoundedBorder()
+	s.BorderColor = "#874BFD"
 
 	return s
 }
 
 type scout struct {
-	styles *Styles
+	styles    *Styles
+	termWidth int
+	cwd       string // current working directory
+	browser   tea.Model
 }
 
 func (s scout) Init() tea.Cmd {
@@ -47,18 +50,43 @@ func (s scout) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return s, nil
 }
 
-func (s scout) View() string {
-	return "ok"
+func (s scout) headerView() string {
+	return s.cwd
 }
 
-func NewScout() *scout {
+func (s scout) fileBrowser() string {
+	return s.styles.App.
+		BorderStyle(s.styles.FileBrowserBorder).
+		BorderForeground(s.styles.BorderColor).
+		Width(s.termWidth).
+		Render("File1\nFile2\nFile3")
+}
+
+func (s scout) View() string {
+	b := strings.Builder{}
+	b.WriteString(s.headerView())
+	b.WriteRune('\n')
+	b.WriteString(s.fileBrowser())
+	return s.styles.App.Render(b.String())
+}
+
+func NewScout(cwd string, termWidth int) *scout {
 	return &scout{
-		styles: DefaultStyles(),
+		styles:    DefaultStyles(),
+		cwd:       cwd,
+		termWidth: termWidth - 2,
 	}
 }
 
 func main() {
-	p := tea.NewProgram(NewScout(), tea.WithAltScreen())
+	tw, _, _ := term.GetSize(int(os.Stdout.Fd()))
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := tea.NewProgram(NewScout(cwd, tw), tea.WithAltScreen())
 	if err := p.Start(); err != nil {
 		log.Fatal(err)
 	}
